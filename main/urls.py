ecbd02cd3ad75b717a6354ad62be6caf8bc2c30c
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-02-07 16:18:07 +0530

    added marathi language support

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name="english_news"),
-    path('hindi/',views.hindi_news,name="hindi_news")
+    path('hindi/',views.hindi_news,name="hindi_news"),
+    path('marathi/',views.marathi_news,name = "marathi_news"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,41 +4,25 @@ import bs4
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
 
-import requests
-import re
-
-
-
-'''Image search'''
-
-def get_url_images_in_text(html, protocol):
-    urls = []
-    all_urls = re.findall(r'((http\:|https\:)?\/\/[^"\' ]*?\.(png|jpg))', html, flags=re.IGNORECASE | re.MULTILINE | re.UNICODE)
-    for url in all_urls:
-        if not url[0].startswith("http"):
-            urls.append(protocol + url[0])
-        else:
-            urls.append(url[0])
-    return urls
-def get_images_from_url(url):
-    protocol = url.split('/')[0]
-    resp = requests.get(url)
-    return get_url_images_in_text(resp.text, protocol)
-
-
-
 def generate_link(search_keyword):
     #https://news.google.com/rss/search?cf=all&hl=en-IN&q= 
     link_part_1 = 'https://news.google.com/news/feeds?cf=all&ned=in&hl=en&q='
-    search_keyword = search_keyword.replace(" ","")
+    search_keyword = search_keyword.replace(" ","+")
     link = link_part_1+search_keyword+'&output=rss'    
     return link
 
 def generate_hindi_link(search_keyword):
     link_part_1 = 'https://news.google.com/news/feeds?cf=all&ned=in&hl=hi&q='
-    search_keyword = search_keyword.replace(" ","")
+    search_keyword = search_keyword.replace(" ","+")
     link = link_part_1+search_keyword+'&gl=IN&ceid=IN:hi'    
     return link
+def generate_marathi_link(search_keyword):
+    link_part_1 = 'https://news.google.com/news/feeds?cf=all&ned=in&hl=mr&q='
+    search_keyword = search_keyword.replace(" ","+")
+    link = link_part_1+search_keyword+'&gl=IN&ceid=IN:mr'    
+    return link
+    #https://news.google.com/rss/search?cf=all&hl=mr&q=news&gl=IN&ceid=IN:mr
+
 def get_news(url):
     '''news_link = "https://news.google.com/news/rss"'''
     
@@ -67,3 +51,10 @@ def hindi_news(request):
         search_word = 'world'    
     news = get_news(generate_hindi_link(search_word))
     return render(request,'hindi.html',{'news_list' : news,'keyword':search_word})
+def marathi_news(request):
+    if 'search_word' in request.POST:
+        search_word = request.POST['search_word']
+    else:
+        search_word = 'world'    
+    news = get_news(generate_marathi_link(search_word))
+    return render(request,'marathi.html',{'news_list' : news,'keyword':search_word})
